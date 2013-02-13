03485aa7d13ce45fc563b0f760d251857bd692a6
dryman <idryman@gmail.com>
2013-02-12 18:24:31 -0800

    smart-window-move ok

--- smart-window.el
+++ smart-window.el
@@ -27,21 +27,42 @@
 ;; commands
 ;; sw-split  need more design to be vim-like
 ;; sw-vsplit
+;; sw-buffer-split
+;; sw-buffer-vsplit
 ;; sw-move (left, right, above, below)
 
-(defun sw-move-i (arg)
-  (let ((window (selected-window))
-        (dir (intern arg)))
-    (select-window (split-window (frame-root-window) nil dir))
+;; (defvar smart-window-mode-hook nil)
+
+;; (defvar smart-window-mode-map
+;;   (let ((smart-window-map (make-keymap)))
+;;     (define-key 
+
+(defgroup smart-window nil
+  "vim-like window controlling system"
+  :group nil
+  :prefix "smart-window")
+
+;;;###autoload
+(defun smart-window-move (dir)
+  (interactive 
+   (list 
+    (completing-read "Move window: (left/right/above/below) "
+                     '(("left" 1) ("right" 2) ("above" 3) ("below" 4)))))
+  (let ((window (selected-window)))
+    (select-window (split-window (frame-root-window) nil (intern dir)))
     (delete-window window)))
 
-(defun sw-move ()
-  (sw-move-i
-   (completing-read "Move window: (left/right/above/below) "
-                    '(("left" 1) ("right" 2) ("above" 3) ("below" 4)))))
+
+
+;(defun smart-window-split ()
+;  (interactive))
+
+;(defun sw-move-i (arg)
 
 
 
+
+(provide 'smart-window)
 
 
 ;;; smart-window.el ends here
